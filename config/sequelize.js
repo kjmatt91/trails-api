58785118ddec16ae5a1b61f00ca6836fcc2190bd
kjmatt91 <kjmatt91@gmail.com>
2021-10-17 00:31:21 -0400

    set up migration files

--- config/sequelize.js
+++ config/sequelize.js
@@ -4,6 +4,6 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
   }
 }
